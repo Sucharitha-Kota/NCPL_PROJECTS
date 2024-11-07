79208960b676867271bc8e37e193dd7005c39fc2
Sucharitha-Kota <90026996+Sucharitha-Kota@users.noreply.github.com>
2024-11-07 18:50:52 -0500

    Create SQLqueryforModule2.sql

---
+++ SQLqueryforModule2.sql
@@ -0,0 +1,34 @@
+--query 1
+SELECT * from customer
+
+--query 2
+SELECT CustomerID, FirstName, LastName, ModifiedDate
+FROM customer
+ORDER BY ModifiedDate DESC;
+
+--query 3
+SELECT SalesPerson, COUNT(CustomerID) AS CustomerCount
+FROM customer
+WHERE ModifiedDate > '"2005-01-01"'
+GROUP BY SalesPerson;
+
+--query 4
+SELECT CustomerID, FirstName, LastName 
+FROM customer
+WHERE LastName LIKE '%Haines%';
+
+--query 5
+SELECT CustomerID, FirstName, LastName, EmailAddress
+FROM customer
+WHERE EmailAddress LIKE '"%@adventure-works.com"';
+
+--query 6
+SELECT CustomerID, FirstName, LastName, ModifiedDate
+FROM customer
+ORDER BY ModifiedDate DESC;
+
+--query 12
+SELECT CustomerID, FirstName, LastName, rowguid
+FROM customer
+WHERE rowguid ='3f5ae95e-b87d-4aed-95b4-c3797afcb74f';
+
